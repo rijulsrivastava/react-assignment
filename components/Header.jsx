3468e508edd22bc761733761b329657c23a66dba
rijulsrivastava <rijulsrivastva@gmail.com>
2026-07-04 13:01:09 +0530

    Header Component is added

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import './header.css'
+
+function Header(){
+    return(
+        <header className='header-section'>
+            <h1 className='heading'>To Do List</h1>
+        </header>
+    )
+}
+
+export default Header;
